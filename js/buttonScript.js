0ac3203d7964dab5b0c173d09cfbde2e42e8299e
samquigley <samq2011@live.com>
2015-12-08 13:35:26 +0000

    updated button script file

--- js/buttonScript.js
+++ js/buttonScript.js
@@ -16,3 +16,126 @@
 				
 			});
 		    });
+		     $(document).ready(function(){
+			$("#week3").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+			$("#Btn3").click(function(){
+				$("#week3").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+				
+			});
+		    });
+		    $(document).ready(function(){
+			$("#week4").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+			$("#Btn4").click(function(){
+				$("#week4").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+				
+			});
+		    });
+
+ $(document).ready(function(){
+			$("#week5").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+			$("#Btn5").click(function(){
+				$("#week6").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+				
+			});
+		    });
+		    $(document).ready(function(){
+			$("#week6").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+			$("#Btn6").click(function(){
+				$("#week6").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+				
+			});
+		    });
+
+ $(document).ready(function(){
+			$("#week7").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+			$("#Btn7").click(function(){
+				$("#week7").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+				
+			});
+		    });
+		    $(document).ready(function(){
+			$("#week8").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+			$("#Btn8").click(function(){
+				$("#week8").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+				
+			});
+		    });
+
+ $(document).ready(function(){
+			$("#week9").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+			$("#Btn9").click(function(){
+				$("#week9").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+				
+			});
+		    });
+		    $(document).ready(function(){
+			$("#week10").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+			$("#Btn10").click(function(){
+				$("#week10").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+				
+			});
+		    });
+ $(document).ready(function(){
+			$("#week11").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+			$("#Btn11").click(function(){
+				$("#week11").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+				
+			});
+		    });
+		    $(document).ready(function(){
+			$("#week12").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+			$("#Btn12").click(function(){
+				$("#week12").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+				
+			});
+		    });
+
+ $(document).ready(function(){
+			$("#week13").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+			$("#Btn13").click(function(){
+				$("#week13").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+				
+			});
+		    });
+		    $(document).ready(function(){
+			$("#week14").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+			$("#Btn14").click(function(){
+				$("#week14").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+				
+			});
+		    });
+
+ $(document).ready(function(){
+			$("#week15").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+			$("#Btn15").click(function(){
+				$("#week15").fadeToggle("slow");
+				$("img").fadeToggle("slow");
+				
+			});
+		    });
+		   
