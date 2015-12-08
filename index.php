bf40ccce4d25a26b2d7b3d84977d76628527b52e
samquigley <samq2011@live.com>
2015-12-08 17:26:11 +0000

    qdjhk

--- index.php
+++ index.php
@@ -12,30 +12,5 @@ $proc = new XSLTProcessor;
 $proc -> importStyleSheet($xsl); //attach the xsl rules
 
 echo $proc -> transformToXML($xml);
-
-
-if(isset($_REQUEST['ok']))
-{
-   
-    
-    $rootTag =$xml->getElementByTagName("fixtures")->item(0);
-    
-    $FixturesTag = $xml->createElement("week16");
-    $homeTeamTag = $xml->createElement("homeTeam",$_REQUEST['homeTeam']);
-    $awayTeamTag = $xml->createElement("awayTeam",$_REQUEST['awayTeam']);
-    $inputResultTag = $xml->createElement("inputDate",$_REQUEST['inputDate']);
-    $inputDateTag = $xml->createElement("inputResult",$_REQUEST['inputResult']);
-    
-    $homeTeamTag->appendChild($homeTeamTag);
-    $awayTeamTag->appendChild($awayTeamTag);
-    $inputResultTag->appendChild($inputResultTag);
-    $inputDateTag->appendChild($inputDateTag);
-    
-    $rootTag->appendChild($FixturesTag);
-    
-    $xml->save("xml/PL.xml");
-    
-    
-}
-
 ?>
+
